0a49b4b8e3b5e298b044bcd84e731372153185cf
Jiwon <tj1tj2tj3@naver.com>
2024-01-26 16:20:12 +0900

    ex1

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,30 @@ function App() {
   ];
 
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter((student) => {
+    return student.age >= 18;
+  });
+
+  console.log(filteredStudents)
 
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
         {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
+        {
+          filteredStudents.map((item) => {
+            return (<div >
+              이름 : {item.name}
+              나이 : {item.age}
+              성적 : {item.grade}
+            </div>
+            )
+          })}
+
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
-      </ul>
-    </div>
+      </ul >
+    </div >
   );
 }
 
